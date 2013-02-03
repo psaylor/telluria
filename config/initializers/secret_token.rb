dccde29d78777c51998dd57651acd6060682e85c
saylorp <psaylor@mit.edu>
2013-02-03 03:50:36 -0500

    added rest of files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telluria::Application.config.secret_token = 'e8523dfeca37f21984b54451873d6176dc2b2700868197441f71271fbc3870d493ee0c254f89729ab5574d40dd6e06dfc047851f7018d94b8cbc6144069ab9e3'
